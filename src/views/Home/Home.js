2706cf92de0f0a9dcd102976f6443dc4e2855f47
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-08 15:23:28 +0530

    first commit

---
+++ src/views/Home/Home.js
@@ -0,0 +1,53 @@
+import React,{useState,useEffect}from 'react'
+import axios from "axios"
+import "./Home.css"
+
+import NewsArticle from '../../component/NewsArticle/NewsArticle';
+function Home(){
+    const [news,setNews]=useState([])
+    const [searchQuery,setSearchQuery]=useState("pune")
+
+    const loadNews=async() =>{
+try{
+    const response=await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-10-07&to=2023-10-07&sortBy=popularity&apiKey=process.env.{React_APP_API_KEY}`);
+   
+    setNews(response.data.articles)
+}
+
+catch(error){
+    console.log(error)
+}
+}
+    useEffect(() =>{
+        loadNews()
+    },[])
+
+    useEffect(()=>{
+        loadNews()
+    },[searchQuery])
+return (
+    <div>
+        <h1>News App</h1>
+        <input 
+         type="text"
+         className="search-input"
+         value={searchQuery}
+         onChange={(e)=>{
+          setSearchQuery(e.target.value)
+          }}/>
+    <div className="news-container">
+    {
+        news.map((newsArticle,index)=>{
+            const {author,title,description,url,urlToImage,publishedAt}=newsArticle
+
+            return(
+               <NewsArticle author={author} title={title} description={description} url={url}
+               urlToImage={urlToImage} publishedAt={publishedAt}  key={index}/>
+            )
+        })
+    }
+    </div>
+</div>)}
+
+export default Home
+
